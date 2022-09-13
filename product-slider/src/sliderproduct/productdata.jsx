1741dc43b692c4b2474bf103b0378c5d8a865e09
Ali jafari <gnm.jafaria@gmail.com>
2022-09-13 19:19:11 +0430

    add sabadpardakht

--- product-slider/src/sliderproduct/productdata.jsx
+++ product-slider/src/sliderproduct/productdata.jsx
@@ -4,7 +4,7 @@ export const Productdata = [
         id : 1,
         image : 'https://dkstatics-public.digikala.com/digikala-products/5429fc937835d04218662155a84913574138a0dd_1657364960.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
         titel : 'اسباب بازی',
-        price : '300,000',
+        price : 300000,
         rate : '2'
     },
 
@@ -12,7 +12,7 @@ export const Productdata = [
         id : 2,
         image : 'https://dkstatics-public.digikala.com/digikala-products/d4e76e5b966cc7c5ec4f63d5fb8f747901c08238_1615624782.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80',
         titel : 'مایع ظرفشویی',
-        price : '20,000',
+        price : 20000,
         rate : '4'
     },
 
@@ -20,7 +20,7 @@ export const Productdata = [
         id : 3 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/120821624.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'دمبل' ,
-        price : '400,000',
+        price : 400000,
         rate : '1'
     },
 
@@ -28,7 +28,7 @@ export const Productdata = [
         id :4 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/7242c6bd2b1ebd7fdc192232d94d14b824378ce8_1662376760.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'کفش' ,
-        price : '650,000',
+        price : 650000,
         rate : '5'
     },
 
@@ -36,7 +36,7 @@ export const Productdata = [
         id : 5,
         image : 'https://dkstatics-public.digikala.com/digikala-products/a39e19d827895cfddc33318b92e04a6800184ee4_1661338896.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'کرم' ,
-        price : '50,000',
+        price : 50000,
         rate : '3'
     },
 
@@ -44,7 +44,7 @@ export const Productdata = [
         id :6 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/c9a07e7c96701d0f562969bc1650dbdcd044effa_1658296261.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'شامپو' ,
-        price : '150,000',
+        price : 150000,
         rate : '2'
     },
 
@@ -52,7 +52,7 @@ export const Productdata = [
         id : 7,
         image : 'https://dkstatics-public.digikala.com/digikala-products/a1c1a3af5817ab7fb036a58777716f9c1a247f73_1661584350.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'پیراهن' ,
-        price : '70,000',
+        price : 70000,
         rate : '4'
     },
 
@@ -60,7 +60,7 @@ export const Productdata = [
         id :8 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/06eaffde6b6834861675317d5dcbd995a6eff141_1657942232.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'سشوار' ,
-        price : '180,000',
+        price : 180000,
         rate : '5'
     },
 
@@ -68,7 +68,7 @@ export const Productdata = [
         id :9 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/ea283a8e1e6f03d68756b0a09d2e1363dc499b0e_1656572213.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'مانتو' ,
-        price : '700,000',
+        price : 700000,
         rate : '2'
     },
 
@@ -76,7 +76,7 @@ export const Productdata = [
         id :10 ,
         image : 'https://dkstatics-public.digikala.com/digikala-products/d51ec886489714d179fbe7830a18a482b4f1c214_1658560656.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80' ,
         titel : 'کتاب' ,
-        price : '50,000',
+        price : 50000,
         rate : '5'
     } 
 ]
--- product-slider/src/sliderproduct/sliderproduct.jsx
+++ product-slider/src/sliderproduct/sliderproduct.jsx
@@ -14,6 +14,7 @@ import SamplePrevArrow from '../slider/SamplePrevArrow.jsx';
 const Sliderproduct = () =>{
 
    const [product, setproduct] = useState([]);
+   const [pardakht, setpardakht] = useState([]);
 
    useEffect( ()=>{
       setproduct(Productdata)
@@ -28,27 +29,52 @@ const Sliderproduct = () =>{
       prevArrow: <SamplePrevArrow/>
     };
     
+console.log(pardakht);
 
 
     return(
-
+      <>
+      
       <div className='contianer-product'>
 
          <Slider {...settings}>
             {product.map((item,index) =>{ 
                return(
-                  <div className='product' key={index}>
+                  <div className='product' key={item.id}>
                      <img src={item.image} alt="" />
                      <div className="info">
                         <h1> {item.titel} </h1>
                         <span><AiFillStar/> {item.rate}</span>
                         <p>هزارتومان {item.price} </p>
-                        <button>+</button>
+                        <button onClick={()=> setpardakht([...pardakht ,  {productselectorname: item.titel , productselectorprice: item.price , productid: item.id} ])} >+</button>
                      </div>
                   </div>
                )})}
          </Slider>
       </div>
+
+      <div className='contianer-sabad'>
+         
+            {pardakht.map(item =>{
+         
+               return(
+            
+                  <div className='productselect'>
+                        <h2>{item.productselectorprice}</h2>
+                        <p>{item.productselectorname}</p>
+                        <button onClick={() => setpardakht(pardakht.filter(sss => sss.productid !== item.productid))} >-</button>
+                     </div>
+                  
+               )
+            })}
+         
+            
+            <div className='pardakht'>
+               <h2>هزار تومان {pardakht.reduce((sum , current) => sum + current.productselectorprice, 0)}</h2>
+               <button>پرداخت</button>
+            </div>
+      </div>
+  </>
     
     )
 } 
